32a85ca6c93ada6dd15600a06ac02c9f2dc8de28
simon luo <simonluo345@gmail.com>
2022-03-21 21:45:04 -0700

    sorted mostcommon

--- document_analyzer.py
+++ document_analyzer.py
@@ -4,15 +4,13 @@ newlist = []
 with open("document.txt") as infile:
 
     for line in infile:
-        split_it = line.lower().replace(',', '').replace('.', '').split()
+        split_it = line.replace(',', '').replace('.', '').split()
         for i in split_it:
             newlist.append(i)
-    print(newlist)
-
 
     newgood1 = Counter(newlist)
     mostcommon = newgood1.most_common(5)
 
-for x,y in mostcommon:
+for x,y in sorted(mostcommon):
     print(x, end=': ')
     print(y)

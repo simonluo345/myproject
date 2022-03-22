ce698ba6ae34dddfc46542e6b539c9fafc8aed19
simon luo <simonluo345@gmail.com>
2022-03-21 23:02:08 -0700

    correct timeit.py

--- timeit.py
+++ timeit.py
@@ -1,22 +1,23 @@
-from time import time
+import time
 
 
 def calculate_time(func):
 
     def wrap_func(*args, **kwargs):
-        t1 = time()
+        t1 = time.time()
         result = func(*args, **kwargs)
-        t2 = time()
-        print("Total time:", t2 - t1, "s")
+        t2 = time.time()
+        print("Total time:", t2 - t1)
         return result
 
     return wrap_func
 
 
+@calculate_time
 def long_time(n):
     for i in range(n):
         for j in range(100000):
             i * j
-
+    time.sleep(2)
 
 long_time(5)

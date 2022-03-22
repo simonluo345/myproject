04811b3f0364d1a64911aba6ce37247950a37e8d
simon luo <simonluo345@gmail.com>
2022-03-21 23:06:27 -0700

    correct timeit.py again

--- timeit.py
+++ timeit.py
@@ -7,7 +7,7 @@ def calculate_time(func):
         t1 = time.time()
         result = func(*args, **kwargs)
         t2 = time.time()
-        print("Total time:", t2 - t1)
+        print("Total time", t2 - t1)
         return result
 
     return wrap_func
@@ -18,6 +18,5 @@ def long_time(n):
     for i in range(n):
         for j in range(100000):
             i * j
-    time.sleep(2)
 
 long_time(5)

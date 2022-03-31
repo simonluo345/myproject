dde6de5f8039a74397847893b3fe5e7541772ba8
simon luo <simonluo345@gmail.com>
2022-03-30 22:00:44 -0700

    fix issue

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @myobj.route("/", methods=["POST", "GET"])
 def home():
-
     if request.method == "POST":
         flash(request.form["nm"])
-    return render_template("home.html", title="The Blog", city=city_names, name="Lisa")
+    return render_template("home.html", title="The Blog", city=city_names)

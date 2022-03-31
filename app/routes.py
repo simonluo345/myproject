bc52bb29da2035dd9345b94b840b443d15f00ff5
simon luo <simonluo345@gmail.com>
2022-03-30 21:53:35 -0700

    fix cannot import name

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,11 @@ from app import myobj
 from flask import render_template, flash, request
 
 city_names = ["Paris", "London", "Rome", "Tahiti"]
-name = "Lisa"
+
 
 @myobj.route("/", methods=["POST", "GET"])
 def home():
 
     if request.method == "POST":
         flash(request.form["nm"])
-    return render_template("home.html", title="The Blog", city=city_names, name=name)
+    return render_template("home.html", title="The Blog", city=city_names, name="Lisa")

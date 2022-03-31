80693c408e2946c42da27af64be7c1c1eea48a6b
simon luo <simonluo345@gmail.com>
2022-03-31 00:17:13 -0700

    fix form

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
 from app import myobj
-from flask import render_template, flash, request
+from flask import render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-city_names = ["Paris", "London", "Rome", "Tahiti"]
-name = "Lisa"
+
+class LoginForm(FlaskForm):
+    cityname = StringField('City Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @myobj.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "POST":
-        flash(request.form["nm"])
-    return render_template("home.html", title="The Blog", city=city_names, name=name)
+    city_names = ["Paris", "London", "Rome", "Tahiti"]
+    name = "Lisa"
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("{}".format(form.cityname.data))
+    return render_template("home.html", title="The Blog", city=city_names, name=name, form=form)

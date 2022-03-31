f54d026a27be09abf64844ad11c9dac30cc01b16
simon luo <simonluo345@gmail.com>
2022-03-30 21:05:31 -0700

    fix header not found

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import myobj
 from flask import render_template, flash, request
 
-name = "Lisa"
 city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 
@@ -10,5 +9,5 @@ def home():
 
     if request.method == "POST":
         flash(request.form["nm"])
-    return render_template("home.html", title="The Blog", name=name, city=city_names)
+    return render_template("home.html", title="The Blog", city=city_names)
 
